41bd23d21c74b384857bd6f786793a09c222ec89
Alexander Semyenov <semyenov@hotmail.com>
2023-05-29 14:56:16 +0000

    .

--- packages/scraper/src/browser/storage.ts
+++ packages/scraper/src/browser/storage.ts
@@ -1,8 +1,7 @@
-import type { PluginData } from './page'
+import { createClient } from 'redis'
 
-export const storage: Map<string, PluginData> = new Map([
-  ['start', {
-    selector: 'a',
-    urls: ['https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'],
-  }],
-])
+export const publisher = createClient({})
+export const subscriber = publisher.duplicate()
+
+await publisher.connect()
+await subscriber.connect()
