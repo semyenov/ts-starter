f178910855d135e2dd12e1f2a356171c4716551c
Alexander Semyenov <semyenov@hotmail.com>
2023-05-30 14:39:12 +0000

    good

--- packages/scraper/src/browser/storage.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'redis'
-
-export const publisher = createClient({})
-export const subscriber = publisher.duplicate()
-
-await publisher.connect()
-await subscriber.connect()
